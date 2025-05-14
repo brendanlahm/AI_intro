1d4cc84573cbdd3bbba39e0e0b6293a5741243d5
brendanlahm <brendanlahm@gmail.com>
2025-05-14 10:15:36 +0200

    Added code for generating a vector in Exercise 4

--- Exercise4.py
+++ Exercise4.py
@@ -2,10 +2,33 @@
 
 import numpy as np
 
+# Create a vector
 v = np.array([1,2,3,4])
 
+# Determine # x + y values
 w = v.shape
 
+# Create a matrix
 m = np.array([[1,2], [3,4]])
 m
+
+# Print specific value of matrix (row, column)
 print(m[0,1])
+
+# Print datatype of the matrix (int64)
+print(m.dtype)
+
+# Generate a vector based on a range
+m_gen = np.arange(0,13,3)
+m_gen
+
+# sin calculations on a bunch of values
+for x in np.linspace(0, 2*np.pi, 1000):
+    np.sin(x)
+
+
+
+
+
+
+
