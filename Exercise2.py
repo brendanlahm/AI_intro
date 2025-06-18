088769d275265b3b224f27ff8cd446eccbafd4f0
brendanlahm <brendanlahm@gmail.com>
2025-06-18 21:53:21 +0200

    Added a Linear Regression function to Exercise2

--- Exercise2.py
+++ Exercise2.py
@@ -1,12 +1,29 @@
-#######################################
-print("1")
-
+####################################### Machine Learning Practice
+####################### Quick Test
 def print_pattern(n):
     if n > 9:
         return
     for i in range(1, n + 1):
-        print(str(i) * i)
+        print(i)
 
 n = input("give a number: \n")
 n = int(n)
 print_pattern(n)
+
+####################### Linear Regression
+import numpy as np
+
+class LinearRegression:
+    def __init__(self) -> None:
+        self.w = np.zeros((2,))
+
+    def fit(self, data, epochs, lr=0.1):
+        for epoch in range(epochs):
+            pass
+
+
+reg = LinearRegression()
+data = np.array([[1, 1], [2, 3], [4, 3]])
+reg.fit(data, 1, lr=0.1)
+
+
